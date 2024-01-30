1ea6dbe33edbef7152121dc660db482652f653df
Brandon Toapanta <brandontoapanta12345@gmail.com>
2024-01-29 22:22:46 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+import Productos from './components/Productos'
+import Layout from './components/Layout'
+import Title from './components/Title'
+class App extends Component {
+  state = {
+    productos: [
+      { name: 'Tomate', price: 1500, img: '/productos/tomate.png' },
+      { name: 'Arbejas', price: 2500, img: '/productos/arbejas.png' },
+      { name: 'Lechuga', price: 500, img: '/productos/lechuga.png' },
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Title />
+          <Productos
+            agregarAlCarro={() => console.log('no hace nada')}
+            productos={this.state.productos}
+          />
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
